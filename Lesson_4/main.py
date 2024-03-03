27ea31b42f19aa631c328d979d447d95fcac6996
SidorovK <2887458@gmail.com>
2024-03-03 15:13:43 +0300

    change element at tuple

--- Lesson_4/main.py
+++ Lesson_4/main.py
@@ -1,5 +1,13 @@
 import random
 
+def change_elements(numbers, index):
+    return numbers[:index] + (random.randint(1, 10), ) + numbers[index+1:]
+
 numbers = tuple(random.randint(1,10) for _ in range(10))
 
+index = 4
+
 print(numbers)
+numbers = change_elements(numbers, index)
+print(numbers)
+print(type(numbers))
