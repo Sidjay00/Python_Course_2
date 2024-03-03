475ad8eee2d89f9efab9f8a64e2d27f5c87f336e
SidorovK <2887458@gmail.com>
2024-03-03 15:36:14 +0300

    trying to solve task 2

--- Lesson_4/main.py
+++ Lesson_4/main.py
@@ -1,13 +1,21 @@
-import random
+# import random
 
-def change_elements(numbers, index):
-    return numbers[:index] + (random.randint(1, 10), ) + numbers[index+1:]
+# def change_elements(numbers, index):
+#     return numbers[:index] + (random.randint(1, 10), ) + numbers[index+1:]
 
-numbers = tuple(random.randint(1,10) for _ in range(10))
+# numbers = tuple(random.randint(1,10) for _ in range(10))
 
-index = 4
+# index = 4
+
+# Задание 2. На ввод подаются 2 числа, нужно сделать метод, который вернёт произведение, сумму, разность и отношение этих двух чисел
+
+
+def method(a,b):
+    return numbers = (a*b, a+b, a-b, a/b, )
+
+a, b = 10, 2
 
 print(numbers)
-numbers = change_elements(numbers, index)
-print(numbers)
+# numbers = change_elements(numbers, index)
+# print(numbers)
 print(type(numbers))
