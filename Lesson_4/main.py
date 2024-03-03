62c22c4334eadc27f101d855d738b9065cc142e4
SidorovK <2887458@gmail.com>
2024-03-03 21:55:03 +0300

    added the solution for the t_2

--- Lesson_4/main.py
+++ Lesson_4/main.py
@@ -6,16 +6,18 @@
 # numbers = tuple(random.randint(1,10) for _ in range(10))
 
 # index = 4
+# numbers = change_elements(numbers, index)
+# print(numbers)
 
 # Задание 2. На ввод подаются 2 числа, нужно сделать метод, который вернёт произведение, сумму, разность и отношение этих двух чисел
 
 
 def method(a,b):
-    return numbers = (a*b, a+b, a-b, a/b, )
+    return a*b, a+b, a-b, a/b
 
 a, b = 10, 2
-
+numbers = method(a, b)
+summ, diff, multiplication, div = method(a, b)
 print(numbers)
-# numbers = change_elements(numbers, index)
-# print(numbers)
 print(type(numbers))
+print(summ, diff, multiplication, div)
